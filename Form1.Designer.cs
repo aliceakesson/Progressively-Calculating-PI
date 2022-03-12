1d558eb940f77f8dc39b4721d9b941725633dbb0
Alice Åkesson <91065258+aliceakesson@users.noreply.github.com>
2022-03-12 18:43:47 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace Progressively_Calculating_PI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pi_label = new System.Windows.Forms.Label();
+            this.calcTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // pi_label
+            // 
+            this.pi_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pi_label.ForeColor = System.Drawing.Color.White;
+            this.pi_label.Location = new System.Drawing.Point(12, 9);
+            this.pi_label.Name = "pi_label";
+            this.pi_label.Size = new System.Drawing.Size(760, 743);
+            this.pi_label.TabIndex = 0;
+            // 
+            // calcTimer
+            // 
+            this.calcTimer.Enabled = true;
+            this.calcTimer.Interval = 1;
+            this.calcTimer.Tick += new System.EventHandler(this.calcTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(784, 761);
+            this.Controls.Add(this.pi_label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label pi_label;
+        private System.Windows.Forms.Timer calcTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Progressively_Calculating_PI
+{
+    public partial class Form1 : Form
+    {
+
+        bool gameIsRunning = true;
+        int width = 7;
+        int height = 7;
+
+        double amountOfDots = 0;
+        double dotsInCircle = 0;
+
+        int safetyMargin = 500;
+        int index = 0;
+        int k = 0;
+
+        int currentFirstNumber = 0, prevFirstNumber = 0; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.Manual;
+            Location = new Point(50, 50);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calcTimer_Tick(object sender, EventArgs e)
+        {
+
+            #region Preview of value
+
+            Random rnd = new Random();
+            int x = rnd.Next(-400, 400);
+            int y = rnd.Next(-400, 400);
+
+            amountOfDots++;
+
+            double distance = x * x + y * y;
+            if (distance <= 400 * 400)
+                dotsInCircle++;
+
+            double PI = 4 * (double)(dotsInCircle / amountOfDots);
+
+            if (index > 0)
+            {
+                PI *= Math.Pow(10, index-1);
+                PI -= (int)(PI);
+                PI *= 10;
+            }
+
+            Console.WriteLine(PI);
+
+            prevFirstNumber = currentFirstNumber;
+            currentFirstNumber = (int)PI;
+
+            if (currentFirstNumber == prevFirstNumber)
+                k++;
+            else
+                k = 0;
+
+            if (k == safetyMargin)
+            {
+                k = 0;
+                pi_label.Text += currentFirstNumber;
+
+                if(index == 0)
+                   pi_label.Text += ",";
+
+                index++;
+            }
+
+            #endregion
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+            #region Graphic Simulation
+            //Graphics g = e.Graphics;
+            //Pen penSquare = new Pen(Color.Red);
+            //Brush brushSquare = (Brush)Brushes.Red;
+
+            //Pen penCircle = new Pen(Color.Green);
+            //Brush brushCircle = (Brush)Brushes.Green;
+
+            //pi_label.Visible = false; 
+            //Random rnd = new Random();
+
+            //while(gameIsRunning)
+            //{
+            //    int x = rnd.Next(-400, 400);
+            //    int y = rnd.Next(-400, 400);
+
+            //    amountOfDots++;
+
+            //    double distance = x * x + y * y; 
+            //    if(distance > 400*400)
+            //    {
+            //        g.DrawEllipse(penSquare, x + 400, y + 400, width, height);
+            //        g.FillEllipse(brushSquare, x + 400, y + 400, width, height);
+            //    }
+            //    else
+            //    {
+            //        dotsInCircle++; 
+            //        g.DrawEllipse(penCircle, x + 400, y + 400, width, height);
+            //        g.FillEllipse(brushCircle, x + 400, y + 400, width, height);
+            //    }
+
+            //    double PI = 4 * (double)(dotsInCircle/amountOfDots);
+            //    Console.WriteLine(PI);
+
+            //}
+            #endregion
+
+        }
+    }
+}
